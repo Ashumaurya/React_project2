301b9454aea705ef8e285508a4ce87e3d3777cee
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2020-04-03 15:39:32 +0530

    axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
 
-const Home = ()=>{
+class  Home extends Component {
+    state = {
+        posts : [ ]
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res =>{
+           // console.log(res)
+           this.setState({
+               post : res.data.slice(0,5)
+           });
+        })
+    }
+  render(){
     return(
         <div className = "container">
             <h1 className = "center"> Home </h1>
             <p className = "container centre">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consectetur quam quis gravida convallis. Maecenas pretium pretium faucibus. Nullam elementum fringilla elit, ut condimentum ipsum faucibus eu. Donec egestas nibh ornare lectus volutpat, in volutpat turpis malesuada. Nulla sagittis nisl vel ligula tincidunt congue. Ut vel quam egestas, rutrum tellus non, interdum magna. Duis facilisis lacus et orci pulvinar fringilla. Nunc et sapien a purus fermentum faucibus sed pellentesque diam. </p>
         </div>
     )
+  }
 }
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Navlink,Link,withRouter} from 'react-router-dom';
+import {Link,withRouter} from 'react-router-dom';
 
 const Navbar = (props) =>{
-    setTimeout(()=>{            // programatically redirecting to a page
-        props.history.push('/About')
-    },2000) // 2000 ms = 2s
+//   setTimeout(()=>{            // programatically redirecting to a page
+//         props.history.push('/About')
+//     },2000) // 2000 ms = 2s
     return(
         <nav className = "nav-wrapper black darken-3">
             <div className ="container">
                 <Link to = '/' className ="brand-logo"  > PROJECT 2</Link>
                 <ul className = "right">
-                    <li><Link exact to  = '/'>HOME</Link></li> {/*links are used instead of <a> tag so that page doest refreshes and all the request can be handled by th react router dom */}
+                    <li><Link to  = '/'>HOME</Link></li> {/*links are used instead of <a> tag so that page doest refreshes and all the request can be handled by th react router dom */}
                     <li><Link to  = "/Contact">Contact</Link></li>
                     <li><Link to  = "/About">About</Link></li>
 
