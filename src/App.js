5ce8da873a1ad9db0a83678358662ba4d68ce440
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2020-04-03 21:37:12 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import {BrowserRouter , Route} from 'react-router-dom';
+import {BrowserRouter , Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -13,11 +13,12 @@ class App extends Component {
     <BrowserRouter>
         <div className="App">
         <Navbar/>
-        <Route path exact = '/' component ={Home}/>
-        <Route path = '/Contact' component ={Contact}/>
-        <Route path = '/About' component ={About}/>
-        <Route path = '/:post_id' component ={Post}/> {/*this here tells route to post when anything is written after "/" this  */}
-
+        <Switch>
+            <Route path exact = '/' component ={Home}/>
+            <Route path = '/Contact' component ={Contact}/>
+            <Route path = '/About' component ={About}/>
+            <Route path = '/:post_id' component ={Post}/> {/*this here tells route to post when anything is written after "/" this  */}
+        </Switch>
           
         </div>
     </BrowserRouter>
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+
 //class to display post
 class Post extends Component{
     state ={
-        id : null
+       post: null
     }
     componentDidMount(){
         //console.log(this.props);
         let id = this.props.match.params.post_id
-        this.setState({
-            id:id
+        //lets get the post with id 
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + id) // we gave the id of the post we click so the post it will show with the same id 
+        .then(data =>{
+           // console.log(data)
+           this.setState({
+            post : data.data  //*we are setting our post data to the data we get from axios with the specific id  */}
 
         });
+
+        });
+       
     }
     render(){
+        const post = this.state.post?(
+            <div className = "post">
+                <h1 className = "center">{this.state.post.title}</h1>
+                <p className="container">{this.state.post.body}</p>
+            </div>
+        ):(
+            <div className = "center"> Your  post is loading.......</div>
+        )
         return(
             <div className ="container">
-                <h1 className ="center">{this.state.id}</h1>
+               {post}
             </div>
         )
     }
