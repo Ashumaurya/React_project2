1b9504d1260009d08c612bc843b9089fa9375d5c
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2020-04-03 14:49:27 +0530

    First commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = ()=>{
+    return(
+        <div className = "container">
+            <h1 className = "center">About </h1>
+            <p className = "container centre">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consectetur quam quis gravida convallis. Maecenas pretium pretium faucibus. Nullam elementum fringilla elit, ut condimentum ipsum faucibus eu. Donec egestas nibh ornare lectus volutpat, in volutpat turpis malesuada. Nulla sagittis nisl vel ligula tincidunt congue. Ut vel quam egestas, rutrum tellus non, interdum magna. Duis facilisis lacus et orci pulvinar fringilla. Nunc et sapien a purus fermentum faucibus sed pellentesque diam. </p>
+        </div>
+    )
+}
+export default About;
